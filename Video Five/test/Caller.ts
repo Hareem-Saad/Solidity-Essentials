44c9d11bac557cd758b24857ff81c9e5253e360a
Hareem <hareemsaadqsols@gmail.com>
2023-01-12 17:33:43 +0500

    finished

--- Video Five/test/Caller.ts
+++ Video Five/test/Caller.ts
@@ -28,31 +28,10 @@ describe ("Caller", function () {
     it("Should trigger book ticket's fallback", async function () {
       const { contractCaller, owner , contractTicket} = await loadFixture(deployTicketContract);
 
-      // expect(await contractCaller.callFallbackUsingSignature()).to.emit(contractTicket, "logMsgData").withArgs("0x6c6f6c6c7920706f70", 0);
+      expect(await contractCaller.callFallbackUsingSignature()).to.emit(contractTicket, "logMsgData").withArgs("0x6c6f6c6c7920706f70", 0);
 
-      const tsx = await contractCaller.callFallbackUsingSignature()
-      console.log((await tsx.wait()).events?.filter((x) => {return x.event == "logMsgData"}));
+      // const tsx = await contractCaller.callFallbackUsingSignature()
+      // console.log((await tsx.wait()).events?.filter((x) => {return x.event == "logMsgData"}));
     });
   });
 });
-
-
-
-/**
- * const [owner, acc1, acc2] = await ethers.getSigners();
-
-    const ContractTicket = await ethers.getContractFactory("BookTicket");
-    const contractTicket = await ContractTicket.deploy();
-
-    const ContractCaller = await ethers.getContractFactory("Caller");
-    const contractCaller = await ContractCaller.deploy();
-
-    const ContractConstant = await ethers.getContractFactory("Constant");
-    const contractConstant = await ContractConstant.deploy();
-
-    const basicPrice = await contract.basicPrice();
-    const silverPrice = await contract.silverPrice();
-    const goldPrice = await contract.goldPrice();
-
-    return { contractTicket, owner, acc1, acc2, basicPrice, silverPrice, goldPrice, contractCaller, contractConstant };
- */
