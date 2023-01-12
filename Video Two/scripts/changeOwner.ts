3a952376765ae6eb0d1e12c76dc0139fc480add9
Hareem-Saad <122263956+Hareem-Saad@users.noreply.github.com>
2023-01-12 08:54:04 +0500

    finished

--- Video Two/scripts/changeOwner.ts
+++ Video Two/scripts/changeOwner.ts
@@ -17,8 +17,19 @@ async function main() {
   console.log("address: ", lock.address);
 
   //will call from the deployer's address
-  const result =  await lock.changeOwner("0xC39008efF8AdDb27eC71B6afD713Ba9aa636aBE9");
+  // const result =  await lock.changeOwner("0xC39008efF8AdDb27eC71B6afD713Ba9aa636aBE9");
   
+  //To call function from a different address
+  // Connect to the project network
+  let provider = new ethers.providers.JsonRpcProvider(`https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`)
+
+  // Load the wallet to deploy the contract with
+  let wallet = new ethers.Wallet(`0x${process.env.SECOND_PRIVATE_KEY}`, provider);
+  
+  const result =  await lock.connect(wallet).changeOwner("0xC39008efF8AdDb27eC71B6afD713Ba9aa636aBE9");
+  
+  await result.wait();
+
   console.log("Result: ", result);
 }
 
